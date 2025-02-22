f0b9691de56d9b3ac964522e1acc2a16e0cddf9d
linkelumalai <linkelumalai@gmail.com>
2025-02-22 14:35:02 +0530

    init

---
+++ backend/user/src/test/java/com/lucky/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
